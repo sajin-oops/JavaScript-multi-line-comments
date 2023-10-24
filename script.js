7ecd6a3756448f0d1c398d231f35b225fa46a995
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2023-10-24 21:30:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let product = {
+  name:"WEBSUIT",
+  price : 100000,
+  owner:"sajin"
+
+}
+console.log(product)
+
+/*
+{name: 'WEBSUIT', price: 100000, owner: 'sajin'}
+name
+: 
+"WEBSUIT"
+owner
+: 
+"sajin"
+price
+: 
+100000
+[[Prototype]]
+: 
+Object
+*/
